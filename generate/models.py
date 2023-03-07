24dee2690022f561c43f856c683202e014fcd3ef
milamin <aminsasa187@gmail.com>
2023-03-07 23:44:45 +0200

    Create model and added to admin panel

--- generate/models.py
+++ generate/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    job = models.CharField(max_length=100)
+    email = models.EmailField()
+    domain = models.URLField()
+    phone_number = models.CharField(max_length=20)
+    company_name = models.CharField(max_length=100)
+    text = models.TextField()
+    integer = models.IntegerField()
+    address = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
